54e13925f454c76cce03c7d1da27f0e4760d3701
shai9 <dennis.shakava@student.moringaschool.com>
2022-09-28 06:24:18 +0300

    Done

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,8 +1,10 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
-
+      
       t.timestamps
     end
   end
 end
+
+#   add_description_to_products description:string
